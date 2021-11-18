a0ffb379d84b34e34b6eae36380a0aa6d79ec2c1
Caroline <anacdolima@gmail.com>
2021-11-18 14:51:37 -0300

    Informações Básicas do Quiz

---
+++ src/scripts.js
@@ -0,0 +1,33 @@
+// Passar para a página seguinte
+// Dina fez isso em uma aula do acelera aí
+function nextPage(classPageA, classPageB){
+    const pageA = document.querySelector(classPageA);
+    const pageB = document.querySelector(classPageB);
+
+    pageA.classList.add("hide");
+    pageB.classList.remove("hide");
+}
+
+function verifyQuizzInfo(classPageA, classPageB){
+    const inputTitle = document.querySelector(".quizz-title").value;
+    const inputURL = document.querySelector(".quizz-url").value;
+    const inputNumberOfQuestions = document.querySelector(".quizz-num-questions").value;
+    const inputNumberOfLevels = document.querySelector(".quizz-num-levels").value;
+    let n = 0;
+
+    if(inputTitle.length < 20 || inputTitle.length > 65){
+        alert("Título do Quizz deve ter entre 20 e 65 caracteres");   
+    } else n++;
+    if(inputURL){
+
+    }else n++;
+    if(inputNumberOfQuestions < 3 ){
+        alert("São necessárias no mínimo 3 perguntas");   
+    }else n++;
+    if(inputNumberOfLevels < 2){
+        alert("São necessários no mínimo 2 níveis");   
+    }else n++;
+
+    if(n === 4) 
+        nextPage(classPageA, classPageB);
+}
